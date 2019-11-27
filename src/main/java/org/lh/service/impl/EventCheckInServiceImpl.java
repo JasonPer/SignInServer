d5e976f866b88ee8e5757afbe667ed6b1b4f8687
JasonPer <m15191678056@163.com>
2019-11-27 19:05:23 +0800

    完成了工作人员扫码签到相关接口

--- src/main/java/org/lh/service/impl/EventCheckInServiceImpl.java
+++ src/main/java/org/lh/service/impl/EventCheckInServiceImpl.java
@@ -15,7 +15,6 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.Optional;
 
 /**
